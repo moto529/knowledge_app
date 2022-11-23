272dc4a68a0ccee93f897dbc6fbe18a2ffe3c28f
moto529 <mo5to29ki@gmail.com>
2022-11-24 00:07:35 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :knowledgess
+  
+  has_many :knowledges
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable

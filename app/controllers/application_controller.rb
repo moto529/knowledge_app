3fe7dbe04445dde778058a0572bab59e1f016d2d
moto529 <mo5to29ki@gmail.com>
2022-11-23 17:31:58 +0900

    user追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :config_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+  
+  private
+  def config_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name birth_date])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name birth_date])
+  end
 end

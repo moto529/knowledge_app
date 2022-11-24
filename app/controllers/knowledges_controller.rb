6ca73b98a8f814110918855b1ab114b70f68ff1d
moto529 <mo5to29ki@gmail.com>
2022-11-24 23:31:38 +0900

    indexとshow追加

--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -1,20 +1,43 @@
 class KnowledgesController < ApplicationController
   before_action :authenticate_user!
+  
+  def index
+    @knowledges = Knowledge.where(user_id: current_user.id).order(created_at: "desc")
+  end
+  
   def new
     @knowledge = Knowledge.new
-    @knowledges = Knowledge.all
   end
   
   def create
     @knowledge = Knowledge.new(knowledge_params)
     @knowledge.user = current_user
     if @knowledge.save
-      redirect_to new_knowledge_path
+      redirect_to knowledges_path, notice: "ナレッジを作成しました。"
     else
       @knowledges = Knowledge.all
       render "knowledges/new", status: :unprocessable_entity
     end
   end
+  
+  def show
+    @knowledge = Knowledge.find(params[:id])
+  end
+  
+  def update
+    @knowledge = Knowledge.find(params[:id])
+    if @knowledge.update(knowledge_params)
+      redirect_to knowledges_path, notice: "ナレッジを編集しました。"
+    else
+      render "knowledges/show", status: :unprocessable_entity
+    end
+  end
+  
+  # def destroy
+  #   @knowledge = Knowledge.find(params[:id])
+  #   @knowledge.destroy
+  #   redirect_to knowledges_path, notice: "削除しました。"
+  # end
   
   private
   

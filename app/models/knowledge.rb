610b5c0073cb1979435aeb81d80180ae4b2530c1
moto529 <mo5to29ki@gmail.com>
2022-11-24 23:32:47 +0900

    バリデーション追加

--- app/models/knowledge.rb
+++ app/models/knowledge.rb
@@ -2,7 +2,7 @@ class Knowledge < ApplicationRecord
   belongs_to :category
   belongs_to :user
   
-  validates :title, presence: true, length: { maximum: 30 }
+  validates :title, presence: true, length: { maximum: 30 }, uniqueness: { scope: [:category_id, :user_id] }
   validates :body, presence: true
   # validates :url, format: /\A#{URI::regexp(%w(http https))}\z/
   validates :category_id, presence: true

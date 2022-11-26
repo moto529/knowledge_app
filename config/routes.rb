b1a8682cc60cc86ee27d69b2f6ebd05f6652ba59
moto529 <mo5to29ki@gmail.com>
2022-11-26 21:20:26 +0900

    ルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'knowledges#timeline'
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   
   resources :categories, only: %i[index create]
-  resources :knowledges, only: %i[index new create show update destroy]
+  resources :knowledges, only: %i[index new create show update destroy] do
+    get 'timeline', on: :collection
+  end
 end

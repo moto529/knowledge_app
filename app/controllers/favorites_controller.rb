db474ddec685a83cf2262ef5ffc169a393d44df8
moto529 <mo5to29ki@gmail.com>
2022-12-10 15:48:15 +0900

    favorite作成

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,16 @@
+class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    knowledge = Knowledge.find(params[:knowledge_id])
+    current_user.favorite(knowledge)
+    redirect_back fallback_location: root_path
+  end
+
+  def destroy
+    knowledge = current_user.favorites.find(params[:id]).knowledge
+    current_user.unfaborite(knowledge)
+    redirect_back fallback_location: root_path
+  end
+  
+end

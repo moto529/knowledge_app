0d1bfba87ff7089a857f5b0aed2be09aac2e12b9
moto529 <mo5to29ki@gmail.com>
2022-12-12 19:34:34 +0900

    パスワード変更メール設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,10 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+    
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.default_url_options = { host: 'f06af84733f84d3da3940388cd592475.vfs.cloud9.ap-northeast-1.amazonaws.com', port: $PORT, protocol: 'https'  }
+  # config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -60,6 +64,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true

f5d3346bb824ffd50277da25cc96080fea95c7c5
moto529 <mo5to29ki@gmail.com>
2022-12-23 22:30:54 +0900

    修正

--- app/models/knowledge.rb
+++ app/models/knowledge.rb
@@ -29,10 +29,10 @@ class Knowledge < ApplicationRecord
         visited_id: visited_id,
         action: 'comment'
       )
-      # 自分の投稿に対するコメントの場合は、通知済みとする
-      if notification.visitor_id == notification.visited_id
-        notification.checked = true
-      end
-      notification.save if notification.valid?
+    # 自分の投稿に対するコメントの場合は、通知済みとする
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
   end
 end
